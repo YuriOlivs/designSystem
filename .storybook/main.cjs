b34c8ffa454c8d24272124d07ffeeff209ebef50
YuriOlivs <yuri.os.2004@outlook.com>
2022-10-14 16:58:27 -0300

    add publish storybook worflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/designSystem/'
+    }
+    return config 
   }
 }
